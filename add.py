8d2fb3df5cc21ed0afd34faf4a9a0c30138af4a1
Thiru™ <saritha02.03.2005@gmail.com>
2025-05-10 15:47:17 +0530

    new1

--- add.py
+++ add.py
@@ -1,7 +1,7 @@
 y=int(input("enter the number:"))
 X=input("enter the name:")
 b=int(input("enter the age"))
-if y%4==0:
+if y%2==0:
     print("given year is leap")
 else:
     print("not a leap year")
